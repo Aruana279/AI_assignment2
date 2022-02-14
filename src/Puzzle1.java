aba21142799fd6f29d8533d0c2d803b68acdcdad
sheehy98 <sheehy98@gmail.com>
2022-02-13 23:47:15 -0500

    Puzzle 1 comments

--- src/Puzzle1.java
+++ src/Puzzle1.java
@@ -11,32 +11,34 @@ public class Puzzle1 {
 
     public int ga(List<Float> numbers, int seconds) {
         Random random = new Random();
+        // Grabs the time when execution starts
         long startTime = System.currentTimeMillis() / 1000;
+        // Population
         int size = 10;
+        // Top 20% are saved through elitism, bottom 30% are culled
         int NUMSAVED = (int) Math.floor(0.2 * (double) size);
         int NUMREMOVED = (int) Math.floor(0.3 * (double) size);
         Population population = new Population(size);
         population.initializePopulation(numbers);
         int generationCount = 0;
         while (System.currentTimeMillis() / 1000 < startTime + seconds) {
-            // while (generationCount < 10) {
             System.out.println("\nGENERATION " + generationCount);
-            // Get fittest individuals from elitism and use those for the next generation
+            // Get fittest individuals from elitism and save them for the next generation
             List<Individual> topPerformers = elitism(population, NUMSAVED);
+            // Cull the worst performers
             culling(population, NUMREMOVED);
-            // Do crossover on remaining individuals and fittest individuals
-//            System.out.println("Parents after culling: " + population.individuals.size());
+            // Do crossover using all unculled performers as the parent-pool
             population.setIndividuals(crossover(size - NUMSAVED, population.getIndividuals(), numbers));
 
+            // 30% of the performers experience mutation
             for (Individual individual : population.getIndividuals()) {
-                if (random.nextInt(10) < 3){
+                if (random.nextInt(10) < 3) {
                     individual.mutation();
                 }
             }
-
-//            System.out.println("Children after crossover: " + population.individuals.size());
+            // Elite individuals from previous generation are re-added to the pool of
+            // performers
             population.getIndividuals().addAll(topPerformers);
-//            System.out.println("Children after elitism: " + population.individuals.size());
             System.out.println("Best Score: " + elitism(population, 1).get(0).calculateFitness());
             generationCount++;
         }
@@ -45,18 +47,14 @@ public class Puzzle1 {
 
     public void culling(Population population, int numRemoved) {
         List<Float> fitnessScores = new ArrayList<>();
+        // Adds the fitness scores of every performer to a list
         for (Individual individual : population.getIndividuals()) {
             fitnessScores.add(individual.calculateFitness());
         }
-//        for (float num : fitnessScores) {
-//            System.out.print(num + " ");
-//        }
-//        System.out.println();
+        // Removes the worst performer, iterates numRemoved times
         for (int i = 0; i < numRemoved; i++) {
             float min = Collections.min(fitnessScores);
-            
             fitnessScores.remove(min);
-//            System.out.println("Removed: " + min);
         }
         // Get the individuals whose scores weren't removed
         List<Individual> individuals = new ArrayList<>();
@@ -72,33 +70,43 @@ public class Puzzle1 {
 
     public List<Individual> elitism(Population population, int numSaved) {
         List<Float> fitnessScores = new ArrayList<>();
-
+        // Grabs the fitness scores of all performers
         for (Individual individual : population.getIndividuals()) {
             fitnessScores.add(individual.calculateFitness());
         }
         List<Individual> topIndividuals = new ArrayList<>();
         for (int i = 0; i < numSaved; i++) {
+            // Grabs the best score
             float max = Collections.max(fitnessScores);
+            // Finds the corresponding performer and saves it
             for (Individual individual : population.getIndividuals()) {
                 if (individual.getFitness() == max) {
                     topIndividuals.add(individual);
                     break;
                 }
             }
+            // Removes the performer so that the next best performer can be found on the
+            // next iteration
             fitnessScores.remove(new Float(max));
         }
         return topIndividuals;
     }
 
     public List<Individual> crossover(int size, List<Individual> individuals, List<Float> numbers) {
+        // Stores the children that are created
         List<Individual> result = new ArrayList<>();
         int numChildren = 0;
+        // Grabs the score of every performer
         List<Float> defaultScores = new ArrayList<>();
         for (int i = 0; i < individuals.size(); i++) {
             defaultScores.add(individuals.get(i).calculateFitness());
         }
+        // Grabs the minimum score (or 0 if the minimum is above 0)
         Float minScore = Collections.min(defaultScores);
-        if (minScore > 0) {  minScore = new Float(0);  }
+        if (minScore > 0) {
+            minScore = new Float(0);
+        }
+        // Creates the list with gaps corresponding to performer quality
         List<Float> fitnessScores = new ArrayList<>();
         float fitnessSum = 0;
         for (int i = 0; i < individuals.size(); i++) {
@@ -106,9 +114,13 @@ public class Puzzle1 {
             fitnessSum += tempFit;
             fitnessScores.add(fitnessSum);
         }
+        // Creates children until no more are required
         while (numChildren < size) {
             Random random = new Random();
             int parent1Index = 0;
+            // Generates 2 random numbers >= 0 and < largest value in fitnessScores
+            // Selects the parents by finding the first number in fitnessScores greater than
+            // the random numbers
             float parent1Prob = random.nextFloat() * fitnessScores.get(fitnessScores.size() - 1);
             for (int i = 0; i < individuals.size(); i++) {
                 if (fitnessScores.get(i) >= parent1Prob) {
@@ -126,12 +138,15 @@ public class Puzzle1 {
                     }
                 }
             }
+            // Grabs the chosen parents
             Individual parent1 = individuals.get(parent1Index);
             Individual parent2 = individuals.get(parent2Index);
 
             List<Integer> parent1Bins = new ArrayList<>();
             List<Integer> parent2Bins = new ArrayList<>();
 
+            // Randomly chooses the order in which bins from each parents will be paired for
+            // crossover
             int parent1RandomBin = random.nextInt(4) + 1;
             parent1Bins.add(parent1RandomBin);
             while (parent1Bins.size() < 4) {
@@ -150,52 +165,7 @@ public class Puzzle1 {
                 parent2Bins.add(parent2RandomBin);
             }
 
-//            System.out.println("Before crossover");
-//            System.out.println("Parent 1");
-//            System.out.print("Bin 1: ");
-//            for (float num : parent1.getBin(1)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 2: ");
-//            for (float num : parent1.getBin(2)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 3: ");
-//            for (float num : parent1.getBin(3)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 4: ");
-//            for (float num : parent1.getBin(4)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.println();
-//            System.out.println("Parent 2");
-//            System.out.print("Bin 1: ");
-//            for (float num : parent2.getBin(1)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 2: ");
-//            for (float num : parent2.getBin(2)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 3: ");
-//            for (float num : parent2.getBin(3)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 4: ");
-//            for (float num : parent2.getBin(4)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.println();
-
+            // Creates children that are copies of the parents
             Individual child1 = new Individual();
             Individual child2 = new Individual();
             for (int i = 0; i < 10; i++) {
@@ -203,19 +173,20 @@ public class Puzzle1 {
                 child1.getBin2().add(parent1.getBin(2).get(i));
                 child1.getBin3().add(parent1.getBin(3).get(i));
                 child1.getBin4().add(parent1.getBin(4).get(i));
-                child2.getBin1().add(parent1.getBin(1).get(i));
-                child2.getBin2().add(parent1.getBin(2).get(i));
-                child2.getBin3().add(parent1.getBin(3).get(i));
-                child2.getBin4().add(parent1.getBin(4).get(i));
+                child2.getBin1().add(parent2.getBin(1).get(i));
+                child2.getBin2().add(parent2.getBin(2).get(i));
+                child2.getBin3().add(parent2.getBin(3).get(i));
+                child2.getBin4().add(parent2.getBin(4).get(i));
             }
+            // Generates a random cut point for each pair of bins
             for (int i = 0; i < 4; i++) {
                 int cutPoint = random.nextInt(8) + 1;
-//                System.out.println("cut point: " + cutPoint);
                 for (int j = 0; j < cutPoint; j++) {
                     int parent1Bin = parent1Bins.get(i);
                     int parent2Bin = parent2Bins.get(i);
                     float value1 = parent1.getBin(parent1Bin).get(j);
                     float value2 = parent2.getBin(parent2Bin).get(j);
+                    // Swaps all elements in the bins after the cut point
                     child1.getBin(parent1Bin).set(j, value2);
                     child2.getBin(parent2Bin).set(j, value1);
                 }
@@ -223,56 +194,14 @@ public class Puzzle1 {
             child1 = removeDuplicates(child1, numbers);
             child2 = removeDuplicates(child2, numbers);
 
+            // Saves the newly created children
             result.add(child1);
             result.add(child2);
             numChildren += 2;
-
-//            System.out.println("After crossover");
-//            System.out.println("Child 1");
-//            System.out.print("Bin 1: ");
-//            for (float num : child1.getBin(1)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 2: ");
-//            for (float num : child1.getBin(2)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 3: ");
-//            for (float num : child1.getBin(3)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 4: ");
-//            for (float num : child1.getBin(4)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.println();
-//            System.out.println("Child 2");
-//            System.out.print("Bin 1: ");
-//            for (float num : child2.getBin(1)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 2: ");
-//            for (float num : child2.getBin(2)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 3: ");
-//            for (float num : child2.getBin(3)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.print("Bin 4: ");
-//            for (float num : child2.getBin(4)) {
-//                System.out.print(df.format(num) + " ");
-//            }
-//            System.out.println();
-//            System.out.println();
         }
+        // Drops the last child if an extra is created
+        // All children are created in pairs, so to create an odd number of children,
+        // one must be dropped
         if (numChildren > size) {
             result.remove(result.size() - 1);
         }
@@ -280,14 +209,16 @@ public class Puzzle1 {
     }
 
     public Individual removeDuplicates(Individual child, List<Float> numbers) {
+        // Stores all numbers in the bins
         List<Float> binValues = new ArrayList<>();
-        List<Float> duplicates = new ArrayList<>();
-        List<Float> missing = new ArrayList<>();
         binValues.addAll(child.getBin1());
         binValues.addAll(child.getBin2());
         binValues.addAll(child.getBin3());
         binValues.addAll(child.getBin4());
+        List<Float> duplicates = new ArrayList<>();
+        List<Float> missing = new ArrayList<>();
 
+        // Iterates over every number that should appear, counts the number of times it appears
         for (int i = 0; i < 40; i++) {
             int count = 0;
             for (int j = 0; j < 40; j++) {
@@ -295,6 +226,8 @@ public class Puzzle1 {
                     count++;
                 }
             }
+            // Numbers that do not appear are added to the 'missing' list
+            // Numbers that appear more than once are added to the 'duplicates' list
             if (count == 0) {
                 missing.add(numbers.get(i));
             } else if (count > 1) {
@@ -302,15 +235,15 @@ public class Puzzle1 {
             }
         }
 
-//        System.out.print("Duplicates: " + duplicates.size());
-//        System.out.println(" Missing: " + missing.size());
-//        System.out.println();
-
         int count = 0;
         boolean dupFlag = false;
+        // Iterates over every duplicate
         for (int i = 0; i < duplicates.size(); i++) {
+            // Iterates over every number in the bins
             for (int j = 0; j < binValues.size(); j++) {
                 if (binValues.get(j).equals(duplicates.get(i))) {
+                    // The first time the number is equal to the duplicate, nothing changes but a flag is set
+                    // The second time the number is equal to the duplicate, it is replaced with a missing value
                     if (!dupFlag) {
                         dupFlag = true;
                     } else {
@@ -322,6 +255,7 @@ public class Puzzle1 {
             dupFlag = false;
         }
 
+        // Puts the newly modified bin values into the performer
         child.setBin1(binValues.subList(0, 10));
         child.setBin2(binValues.subList(10, 20));
         child.setBin3(binValues.subList(20, 30));
